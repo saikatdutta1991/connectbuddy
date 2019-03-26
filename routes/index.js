83a558ee6f343110c29b6f77d1ce5dd618767fa9
saikat <saikatdutta1991@gmail.com>
2019-03-26 20:19:26 +0530

    base64sting to image

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function (req, res, next) {
 	res.render('index', { title: process.env.APP_NAME });
 });
 
+
+router.get("/image/:image_string", function (req, res) {
+
+	let base64string = req.params.image_string;
+	let type = base64string.substring("data:image/".length, base64string.indexOf(";base64"))
+	var imagedata = base64string.split(",")[1];
+
+	var img = new Buffer(imagedata, 'base64');
+
+	res.writeHead(200, {
+		'Content-Type': `image/${type}`,
+		'Content-Length': img.length
+	});
+
+	res.end(img);
+});
+
 module.exports = router;
