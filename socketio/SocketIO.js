aecf4adcc527590e0eab365adb4047f9f88a49d5
saikat <saikatdutta1991@gmail.com>
2019-04-02 14:01:32 +0530

    video call socket events done

--- socketio/SocketIO.js
+++ socketio/SocketIO.js
@@ -4,6 +4,8 @@ const Message = require('../models/Message');
 const messageStorage = require('./MessageStorage');
 const User = require('../models/User');
 
+const timeout = 10; //timeout sec for connecting user
+
 io.on('connection', async socket => {
 
     console.log(`A user connected ${socket.id}`);
@@ -17,6 +19,7 @@ io.on('connection', async socket => {
     socket.type = socket.handshake.query.type;
     socket.userid = socket.handshake.query.id;
     let socketRoom = `${socket.type}_${socket.userid}`;
+    socket.room = socketRoom;
     socket.join(socketRoom);
     console.log('user joined to room : ' + socketRoom);
 
@@ -135,6 +138,74 @@ io.on('connection', async socket => {
 
 
 
+
+
+
+    /**
+     * main code flow of video chat
+     */
+    socket.on('connect_for_vc', async ({ calleeType, calleeId }) => {
+
+        /** find callee/user by id */
+        let callee = await User.findOne({ _id: calleeId }).lean().exec();
+        let caller = await User.findOne({ _id: socket.userid }).lean().exec();
+
+
+        //send push message to callee to launch app
+        User.sendPushNotification(callee._id, 'new_video_call', `${caller.name}`, 'New Call', `${caller.name} is calling you. Receive or reject the call.`);
+
+
+        /** check for sometime that user connects to socket or not */
+        let timeTick = 0;
+        let calleeRoom = `${calleeType}_${calleeId}`;
+        let waitForCalleeInterval = setInterval(() => {
+
+            //check for timeout
+            if (timeTick++ == timeout) {
+                io.sockets.in(socket.room).emit('is_connected_vc', { isConnected: false });
+                return clearInterval(waitForCalleeInterval);
+            }
+
+
+            /** check if user is connected to socket means online
+             * then send caller than user is connected_for_vc
+             */
+            if (!Helper.isEmptyRoom(calleeRoom)) { //room is not empty
+
+                console.log('user connected for video call');
+
+                //send socket event back to caller
+                io.sockets.in(socket.room).emit('is_connected_vc', { isConnected: true });
+                return clearInterval(waitForCalleeInterval);
+            }
+
+            console.log('trying to connect user for video call : timetick: ' + timeTick);
+
+
+        }, 1000)
+
+
+    });
+
+    socket.on('send_vc', async (data) => {
+        let calleeRoom = `${data.calleeType}_${data.calleeId}`;
+        io.sockets.in(calleeRoom).emit('incoming_call', data);
+    });
+
+    socket.on('accept_vc', async (data) => {
+        let callerRoom = `${data.callerType}_${data.callerId}`;
+        io.sockets.in(callerRoom).emit('vc_accepted', data);
+    });
+
+    socket.on('reject_vc', async (data) => {
+        let callerRoom = `${data.callerType}_${data.callerId}`;
+        io.sockets.in(callerRoom).emit('vc_rejected', data);
+    });
+
+    socket.on('vc_exchange', async (data) => {
+        let room = `${usertype}_${userid}`;
+        io.sockets.in(room).emit(data.type, data);
+    });
 
 
 
